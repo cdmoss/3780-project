1fc70995a4d5d5bf1f3eef64937679ed66a4ea9a
aidan <aidancampbell123@gmail.com>
2022-11-18 23:31:35 -0700

    added 2 tests 1 is busted

--- SlidingWindow/test/Receiver_test.cpp
+++ SlidingWindow/test/Receiver_test.cpp
@@ -15,4 +15,46 @@ protected:
   }
 };
 
-TEST_F(ReceiverTest, receive) {}
+TEST_F(ReceiverTest, receive_ack_value) {
+  Receiver *r = new Receiver();
+  std::pair<unsigned int, std::set<unsigned int>*> p;
+
+  p = r->receive(0);
+  ASSERT_EQ(p.first, 0);
+
+  p = r->receive(2);
+  ASSERT_EQ(p.first, 0);
+
+  p = r->receive(1);
+  ASSERT_EQ(p.first, 2);
+}
+
+TEST_F(ReceiverTest, receive_wrap_around) {
+
+  std::pair<unsigned int, std::set<unsigned int>*> p;
+
+  p = r->receive(0);
+  ASSERT_EQ(p.first, 0);
+
+  p = r->receive(1);
+  ASSERT_EQ(p.first, 1);
+
+  p = r->receive(2);
+  ASSERT_EQ(p.first, 2);
+
+  p = r->receive(3);
+  ASSERT_EQ(p.first, 3);
+
+  p = r->receive(4);
+  ASSERT_EQ(p.first, 4);
+
+  p = r->receive(5);
+  ASSERT_EQ(p.first, 5);
+
+  p = r->receive(6);
+  ASSERT_EQ(p.first, 6);
+
+  p = r->receive(0);
+  ASSERT_EQ(p.first, 0);
+
+}
