8aa120824219746866ed4410c70ba69bb7e8059a
Mackenzie Broughton <macwbroughton@gmail.com>
2022-11-19 16:46:34 -0700

    modified/commented runtest.sh script to cleanup build folder afterwards

--- SlidingWindow/runtest.sh
+++ SlidingWindow/runtest.sh
@@ -1,5 +1,11 @@
+#build project
 cmake -S . -B build
 cmake --build build
+
+#run tests
 ./build/SlidingWindow_test
 ./build/Receiver_test
 ./build/Sender_test
+
+#cleanup build folder
+rm -r ./build
